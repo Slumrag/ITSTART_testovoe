b988a5fac20a610e45fc4c21ed0503552f94bcb8
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-06 16:14:20 +0300

    feat: add SeminarsSevice

---
+++ src/api/services/index.ts
@@ -0,0 +1,1 @@
+export * from './SeminarService/SeminarService';
---
+++ src/api/types/ApiSeminar.ts
@@ -0,0 +1,8 @@
+export type ApiSeminar = {
+  id: number;
+  title: string;
+  description: string;
+  date: string;
+  time: string;
+  photo: string;
+};
